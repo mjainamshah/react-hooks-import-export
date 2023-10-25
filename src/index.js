9f16f7867e9c5431e79b6a1fa18260ff7dd3a8df
mjainamshah <jainam.shah@student.moringaschool.com>
2023-10-25 17:53:46 +0300

    COMPLETED

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ColoradoStateParks from "./ColoradoStateParks";
+import About from "./components/About";
+import Home from "./components/Home";
 
 ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
+
+function App {
+    return (
+        <div>
+            <About/>
+            <Home/>
+        </div>
+    )
+}
